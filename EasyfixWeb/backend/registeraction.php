f54106b6ea908ed4fa494f950d9c42456210bb77
Pradeepa Sandaruwan <pradeepasandharuwan@gmail.com>
2019-10-07 15:55:49 +0530

    add new login and register form

---
+++ EasyfixWeb/backend/registeraction.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo $_POST['first_name'];
+    echo $_POST['last_name'];
+    echo $_POST['company'];
+    echo $_POST['email'];
+    echo $_POST['phone_number'];
+    echo $_POST['phone_number'];
+    echo $_POST['company'];
+    echo $_POST['email'];
+    
+
+
+
+?>
